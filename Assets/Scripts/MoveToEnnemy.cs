10caa6643dfeb20c688fb9422bb507acf7e89707
Adrien Schockaert <adrienschockaert@Adriens-MacBook-Pro.local>
2022-11-08 18:25:01 +0100

    update IA

---
+++ Assets/Scripts/MoveToEnnemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToEnnemy : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    Color color = Color.yellow;
+    int counter = 0;
+    bool InArena = false;
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        //transform.localPosition = new Vector3(Random.Range(-0.97f, 0.66f), 0.1153f, Random.Range(0.8f,2.09f));
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        if (!InArena)
+        {
+            float moveX = actions.ContinuousActions[0];
+            float moveZ = actions.ContinuousActions[1];
+            float rotateY = actions.ContinuousActions[2];
+            float moveSpeed = 0.5f;
+            transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        }
+
+    }
+
+
+    public override void Heuristic( in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> ContinuousActions = actionsOut.ContinuousActions;
+        ContinuousActions[0] = Input.GetAxisRaw("Horizontal");
+        ContinuousActions[1] = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            print("enter the arena");
+            SetReward(+2f);
+            InArena = true;
+            EndEpisode();
+        }
+        if(other.TryGetComponent<Walls>(out Walls wall))
+        {
+            print("hit the wall");
+            SetReward(-1f);
+            EndEpisode();
+        }
+
+        if(other.TryGetComponent<Tree>(out Tree tree))
+        {
+            print("hit a tree");
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+}
